6a5dff92b820bd96c28d380f735030818bd6a696
Dzmitry <3godas@gmail.com>
2022-10-24 15:21:53 +0300

    refactoring of task2 and adding methods to task3

--- HomeWork/src/home_work2/Task2.java
+++ HomeWork/src/home_work2/Task2.java
@@ -2,27 +2,60 @@ package home_work2;
 
 public class Task2 {
     public static void main(String[] args) {
-        printAllVariables();
-    }
-    private static void printAllVariables() {
-        byte byteNumber = 127;
-        short shortNumber = 32767;
-        int number = 2147483647;
-        long bigNumber = 2147483648L;
-        float decimal = 0.32f;
-        double bigDecimal = 0.33;
-        boolean truth = true;
-        char letter = 'J';
-        String text = "This is new text";
+        byte byteVar = 127;
+        short shortVar = 32767;
+        int intVar = 2147483647;
+        long longVar = 2147483648L;
+        float floatVar = 0.32f;
+        double doubleVar = 0.33;
+        boolean booleanVar = true;
+        char charVar = 'J';
+        String stringVar = "This is new text";
 
-        System.out.println("byte is " + byteNumber);
-        System.out.println("short is " + shortNumber);
-        System.out.println("int is " + number);
-        System.out.println("long is " + bigNumber);
-        System.out.println("float is " + decimal);
-        System.out.println("double is " + bigDecimal);
-        System.out.println("boolean is " + truth);
-        System.out.println("char is " + letter);
-        System.out.println("String is " + text);
+        printByte(byteVar);
+        printShort(shortVar);
+        printInt(intVar);
+        printLong(longVar);
+        printFloat(floatVar);
+        printDouble(doubleVar);
+        printBoolean(booleanVar);
+        printChar(charVar);
+        printString(stringVar);
     }
+    private static void printByte(byte byteVar) {
+        System.out.println("byte is " + byteVar);
+    }
+    private static void printShort(short shortVar) {
+        System.out.println("short is " + shortVar);
+    }
+    private static void printInt(int intVar) {
+        System.out.println("int is " + intVar);
+    }
+    private static void printLong(long intLong) {
+        System.out.println("long is " + intLong);
+    }
+    private static void printFloat(float floatVar) {
+        System.out.println("float is " + floatVar);
+    }
+    private static void printDouble(double doubleVar) {
+        System.out.println("double is " + doubleVar);
+    }
+    private static void printBoolean(boolean booleanVar) {
+        System.out.println("boolean is " + booleanVar);
+    }
+    private static void printChar(char charVar) {
+        System.out.println("char is " + charVar);
+    }
+    private static void printString(String stringVar) {
+        System.out.println("String is " + stringVar);
+    }
+
+
+
+
+
+
+
+
+
 }
--- HomeWork/src/home_work2/Task3.java
+++ HomeWork/src/home_work2/Task3.java
@@ -1,14 +1,94 @@
 package home_work2;
 
 import java.util.Scanner;
-
 public class Task3 {
     public static void main(String[] args) {
-        System.out.println(printInput());
+        String template = "Java automation";
+        String startWith = "Java";
+        String endWith = "automation";
+        String find = "auto";
+        String add = " QA";
+        String replace = "C#";
 
+        System.out.println("Enter the name of your course:");
+        Scanner scan = new Scanner(System.in);
+        String text = scan.nextLine();
+
+        textLength(text);
+        textEmpty(text);
+        textCharAt(text);
+        textEquals(text,template);
+        textIgnoreEquals(text,template);
+        textStartsWith(text,startWith);
+        textEndsWith(text,endWith);
+        textContain(text,find);
+        textConcat(text,add);
+        textReplace(text,startWith,replace);
+        textLowCase(text);
+        textFormat(text);
     }
-    private static String printInput() {
-        Scanner input = new Scanner(System.in);
-        return input.nextLine();
+    private static void textLength(String text) {
+        System.out.println("Length of text is " + text.length());
     }
+    private static void textEmpty(String text) {
+        System.out.println("Text is empty.True or false? : " + text.isEmpty());
+    }
+
+    private static void textCharAt(String text) {
+        System.out.println("Char in the last position of your text is " + text.charAt(text.length()-1) );
+    }
+
+    private static void textEquals(String text,String template) {
+        System.out.println("Your text is equal to " + template + " with case.True or false? : " + text.equals(template));
+    }
+
+    private static void textIgnoreEquals(String text,String template) {
+        System.out.println("Your text is equal to " + template + " ignoring case.True or false? : " + text.equalsIgnoreCase(template));
+    }
+
+    private static void textStartsWith(String text,String startWith) {
+        System.out.println("Your text starts with " + startWith + ".True or false? : " + text.startsWith(startWith));
+    }
+
+    private static void textEndsWith(String text,String endWith) {
+        System.out.println("Your text ends with " + endWith + ".True or false? : " +text.endsWith(endWith) );
+    }
+
+    private static void textContain(String text, String find) {
+        System.out.println("Your text contains " + find + ".True or false? : " + text.contains(find));
+    }
+
+    private static void textConcat(String text, String add) {
+        System.out.println("Your text is concat with " + add + " into " + text.concat(add));
+    }
+
+    private static void textReplace(String text,String startWith,String replace) {
+        System.out.println("Your text is replaced with " + replace + " into " + text.replace(startWith,replace));
+    }
+
+    private static void textLowCase(String text) {
+        System.out.println("Your text in the lower case is " + text.toLowerCase());
+    }
+
+    private static void textFormat(String text) {
+        System.out.println("Your text as variable for formatted String is " + String.format("I'm attending %s",text));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
